d440e54478a6f08930e7fed0776b9b660e50cb72
Insaf Ismagilov <insaf.ismagilov@rokolabs.com>
2020-10-18 18:01:52 +0300

    Added user repo.

---
+++ src/SimplePoll.Infrastructure.DataAccess/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using SimplePoll.Domain.Contracts;
+using SimplePoll.Domain.Entities;
+
+namespace SimplePoll.Infrastructure.DataAccess
+{
+	public class UserRepository : IUserRepository
+	{
+		public Task<User> GetAsync(int id)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public Task<int> AddAsync(User user)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public Task<int?> UpdateAsync(User user)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
